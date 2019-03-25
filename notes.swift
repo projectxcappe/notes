c3bf768af5221122910bad0dc7f21475cf581f0f
projectxcappe <projectxcappe@gmail.com>
2019-03-25 08:41:34 -0600

    Create notes.swift

---
+++ notes.swift
@@ -0,0 +1,8 @@
+//Find element index in array by value
+// Initialize the Array
+let arr = ["one", "two", "three", "four"]
+ 
+// Find first occurence of "two" (if any)
+var location = arr.indexOf("two")
+
+print(location!)
